b01ec43ae91207b68e0514e3fa1587b2b089bc3d
Augustin Brocquet <augustinbrocquet@enaco.fr>
2019-09-03 15:28:11 +0200

    add caroussel

--- src/app/pages/backend/wallpappers/wallpapers-list/wallpapers-list.component.ts
+++ src/app/pages/backend/wallpappers/wallpapers-list/wallpapers-list.component.ts
@@ -1,4 +1,6 @@
+import { MockDataModule } from './../../../../@core/mock/mock-data.module';
 import { Component, OnInit } from '@angular/core';
+import { WallpapersService } from 'src/app/shared/services/wallpapers/wallpapers.service';
 
 @Component({
   selector: 'app-wallpapers-list',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WallpapersListComponent implements OnInit {
 
-  constructor() { }
+  public carouselOptions = {
+    margin: 25,
+    nav: true,
+    navText: ['<div class="nav-btn prev-slide"></div>', '<div class="nav-btn next-slide"></div>'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: true
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: false
+      },
+      1500: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  };
+
+  public images: string[] = [];
+
+  constructor(private readonly wallpapersService: WallpapersService) { }
 
   ngOnInit() {
+
+    this.wallpapersService.getInstagramFeed().subscribe((response: any) => {
+
+      response.data.forEach(item => {
+        this.images.push(item.images.standard_resolution.url);
+      });
+
+      console.log(this.images);
+
+    });
+
   }
 
 }
--- src/app/pages/backend/wallpappers/wallpapers-list/wallpapers-list.module.ts
+++ src/app/pages/backend/wallpappers/wallpapers-list/wallpapers-list.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WallpapersListComponent } from './wallpapers-list.component';
-
+import { OwlModule } from 'ngx-owl-carousel';
 
 
 @NgModule({
   declarations: [WallpapersListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    OwlModule
   ]
 })
 export class WallpapersListModule { }
