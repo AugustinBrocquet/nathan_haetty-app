7ce7d9be5428feca63fc51accc5512e3b1c2603b
GozKazu <kriscrinon@gmail.com>
2019-09-23 19:46:44 +0200

    wip

--- src/app/pages/frontend/wallpapers/wallpapers.component.ts
+++ src/app/pages/frontend/wallpapers/wallpapers.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-wallpapers',
   templateUrl: './wallpapers.component.html',
   styleUrls: ['./wallpapers.component.scss']
 })
-export class WallpapersComponent implements OnInit {
+export class WallpapersComponent implements OnInit, AfterViewInit {
 
   public wallpapers: any = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    document.getElementById('nb-global-spinner').style.display = 'none';
   }
 
 }
