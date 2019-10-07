b802fc2e6eee3781929a18365d626974ca86e03d
Augustin Brocquet <augustinbrocquet@enaco.fr>
2019-10-07 12:40:20 +0200

    charge data wallpapers

--- src/app/pages/frontend/wallpapers/wallpapers.component.ts
+++ src/app/pages/frontend/wallpapers/wallpapers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { WallpapersService } from 'src/app/shared/services/wallpapers/wallpapers.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-wallpapers',
@@ -8,10 +10,16 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 export class WallpapersComponent implements OnInit, AfterViewInit {
 
   public wallpapers: any = [];
-
-  constructor() { }
+  public baseUrl = environment.url_api;
+  constructor(private readonly wallpapersService: WallpapersService) { }
 
   ngOnInit() {
+    this.wallpapersService.getWallpapers().subscribe((response: any) => {
+
+      this.wallpapers = response;
+      console.log(this.wallpapers);
+
+    });
   }
 
   ngAfterViewInit() {
